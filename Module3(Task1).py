22b1fcbddd917532865d2032efc9e092402d21c2
RakesPawar2 <rakeshpawar5363@gmail.com>
2025-07-31 20:58:39 +0530

    Assignment 2 Completed

---
+++ Module3(Task1).py
@@ -0,0 +1,9 @@
+#Task 1: Check if a Number is Even or Odd
+N=int(input("Enter a number: ")) #takes a input from user
+
+if N%2!=0:  #(Text Expression):
+    print(N,"is an odd number.") #print is an odd number
+
+else:
+    print(N,"is an even number.") #print is an even number
+
